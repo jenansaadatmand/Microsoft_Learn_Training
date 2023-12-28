ed22c1d8865fb75f039cec9bab1dceda1b8bd8b2
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 16:22:32 -0500

    added a file

---
+++ date_time.py
@@ -0,0 +1,11 @@
+# To work with date : means both calendar date and time in a program
+
+from datetime import date  # use datetime library, import date object from datetime library
+date.today() # use today() function to get today's date
+print(date.today())
+
+# data type conversion, date is a number, cannot be concatinated with a str, 
+# so you need to convert it to str using str()
+print("Today's date is: " + str(date.today()))  
+
+
