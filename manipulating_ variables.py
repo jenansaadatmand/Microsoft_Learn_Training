1958e9bd271d3a808f49c2660b54dd1cd1ec45d3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 16:27:26 -0500

    added a file

---
+++ manipulating_ variables.py
@@ -0,0 +1,28 @@
+# Manipulating string values in variables names:
+
+basaltRockCount = 0
+basaltRockCount = 3
+basaltRockCount = basaltRockCount +1
+print(basaltRockCount)
+basaltRockCount += 3
+print(basaltRockCount)
+
+astronaut = "Stanley G Love"
+upperCase = astronaut.upper()
+print(upperCase)
+
+titleCase = astronaut.title()
+print(titleCase)
+lowerCase = astronaut.lower()
+print(lowerCase)
+capitalized = astronaut.capitalize()
+print(capitalized)
+print(astronaut)
+
+
+
+
+lanchLocationCity = "Cape Canaveral"
+launchLocationState = "Florida"
+concatenate_variables = lanchLocationCity + launchLocationState
+print(concatenate_variables)
