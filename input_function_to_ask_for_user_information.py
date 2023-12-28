cfcbdc5636ee9af5cbc0e3b42565472c97df48bc
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 16:26:55 -0500

    added a file

---
+++ input_function_to_ask_for_user_information.py
@@ -0,0 +1,24 @@
+# Input function to ask for user information
+# store the input value in a variable
+# print the value of the variable back to the user 
+# \n is an escape character, enter new line
+# using print() statements as debugging tools
+
+
+name = input('What is your name: ')
+print('Hello', name)
+print()
+print("Hello")
+print(name)
+print()
+print("Hello\n",name)
+print()
+
+
+
+
+print('adding numbers')
+x = 532 +12
+print('dividing numbers')
+#y = x/0
+print('I did math')
