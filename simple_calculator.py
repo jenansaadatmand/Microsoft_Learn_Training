cad1b2f0600789b8519d463b50184ec8b87d4a6c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 16:29:08 -0500

    added a file

---
+++ simple_calculator.py
@@ -0,0 +1,7 @@
+# A program in python is saved as program.py 
+# A program is a set of multiple instructions or statements to perform action like print something or calculate something
+
+print("This is a simple calculator:") # this will print/show on the console or command line
+x = 1 + 2
+print(x) 
+
