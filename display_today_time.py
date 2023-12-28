bceac17e6833502dc4e03ba4306b6d0681f8a81e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 16:23:12 -0500

    added a file

---
+++ display_today_time.py
@@ -0,0 +1,8 @@
+# A program that displays today's date
+# use date object from the datetime library to access calendar information
+# use the date.today()   use the method today() on the date object
+ 
+from datetime import date 
+print(date.today())
+#print("Today's date is : " + str(date.today())) # convert date into str using str()
+
