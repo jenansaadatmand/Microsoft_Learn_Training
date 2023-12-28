a57c5c45b8bc3a14345e7380e3480b564e777a8b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 16:21:33 -0500

    added a file

---
+++ complex_conditional_logic.py
@@ -0,0 +1,42 @@
+# Complex conditional logic or work flow (if elif elif else)
+# elif: # else if to add multiple test expressions, run in order in which they are written
+
+
+a = 27
+b = 93
+
+if a < b:
+    print("a is less than b")
+elif a > b:
+    print("a is greater than b")
+else:
+    print("a is equal to b")
+
+# Syntax of if elif else conditional statement
+# if test expression:
+    # indented statement to be Run
+# elif expression:
+    # indented statement to be run
+# elif expression:
+    # indented statement to be run
+# else no test expression is needed, it the left over:
+    # indented statement to be run 
+     
+        
+# nested conditional logic:
+# You can nest if, elif, else statements to create even more complex programs
+# to nest conditions, indent the inner conditions
+# everything in at the same level of indentation will be run in the same code block
+
+a = 16
+b = 25
+c = 27
+if a > b:
+    if b > c:
+        print("a is greater than b and b is greater than c")
+
+elif a == b:
+    print("a is equal to b")
+else:
+    print("a is less than b")
+            
