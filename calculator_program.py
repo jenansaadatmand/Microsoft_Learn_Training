c94e59944c7a9e443e8ab3736b1502e310258bc8
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 16:21:07 -0500

    added a file

---
+++ calculator_program.py
@@ -0,0 +1,5 @@
+# Calculator program using input():
+print("Calculator program")
+first_number = input("first number: ")
+second_number = input("Second number: ")
+print(int(first_number) + int(second_number)) 
