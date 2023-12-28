9cb7df904a53a283b6da4cfc27d24d556e0d92af
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 16:20:14 -0500

    added a file

---
+++ calculate_distance_to_moon.py
@@ -0,0 +1,6 @@
+# A Program to calculate how many miles (distance) until rocket reaches moon
+distanceToMoon = 238855
+distanceTraveled = 10234
+
+milesToMoon = distanceToMoon - distanceTraveled
+print(milesToMoon)
