70afdb509f213cece480eff1b04bf3a2782ed797
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 16:19:32 -0500

    added a file

---
+++ Building_calculator_from _Microsoft.py
@@ -0,0 +1,9 @@
+# Building a calculator from Microsoft learn class introduction to python
+# the characters ;\, at the end of the first two statements signal that there are many lines of code
+# using these characters lets you input all of the code, line by line 
+
+
+first_number = int(input('Type the first number: ')) ;\
+second_number = int(input('Type the second number: ')) ;\
+print("The sum is: ", first_number + second_number)
+
