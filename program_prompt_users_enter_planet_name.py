2b17a77890338caa2217a3cc22f569fb1e7c5f86
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:33:49 -0500

    Add files via upload

---
+++ program_prompt_users_enter_planet_name.py
@@ -0,0 +1,15 @@
+# A program to prompt users to enter a list of planet names and displays the planets one by one. 
+
+new_planet = ' '
+planets = []
+while new_planet.lower() != 'done':
+    if new_planet:  # Check if there is a value in new_planet variable
+        planets.append(new_planet)
+    new_planet = input("Enter a new value or done if done: ")
+
+
+# Display the list of planets using a for loop: 
+        
+for new_planet in planets:   
+    print(new_planet)
+
