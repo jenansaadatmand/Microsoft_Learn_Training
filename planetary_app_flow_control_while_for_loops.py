d40af999dd93a793bc68a3b392fcf8c9c130f21f
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:36:06 -0500

    Update planetary_app_flow_control_while_for_loops.py

--- planetary_app_flow_control_while_for_loops.py
+++ planetary_app_flow_control_while_for_loops.py
@@ -81,6 +81,6 @@ while True:
     print(user_input)
     if user_input.lower() == 'done':
         break
-print(list_values) # ask program to print the list on screen, notice this list does not have a check if the variable is empty so it does not include a ' ' empty result. 
+print(list_values) # Ask program to print the list on screen, notice this list does not have a check if the variable is empty so it does not include a ' ' empty result. 
 print()
 print()
