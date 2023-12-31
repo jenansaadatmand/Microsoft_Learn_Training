471d2fc6b640c443592b22f30a2dd78906c0ab93
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:33:34 -0500

    Add files via upload

---
+++ program_count_down_from_4_to_0.py
@@ -0,0 +1,20 @@
+# A program that uses a 'for' loop to count down from 4 to 0:
+
+countdown = [4, 3, 2, 1, 0]
+for number in countdown:
+    print(number)
+print("Blast off!! 🚀")  # added an emoji that I copied from the online lesson in the browser
+print()
+
+
+# Lets change the code to wait one second between each number by using the sleep() function from the time library 
+
+
+from time import sleep  # import the sleep() from the time module 
+
+countdown = [4, 3, 2, 1, 0]
+for number in countdown:
+    print(number)
+    sleep(1)  # wait 1 second 
+    
+print("Blast off!! 🚀")
