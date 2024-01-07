3869062145c9084d845cde8166d75e1bb3a0f693
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-07 11:25:53 -0500

    Update Building_calculator_from _Microsoft.py

--- Building_calculator_from _Microsoft.py
+++ Building_calculator_from _Microsoft.py
@@ -1,6 +1,6 @@
 # Building a calculator from Microsoft learn class introduction to python
 # the characters ;\, at the end of the first two statements signal that there are many lines of code
-# using these characters lets you input all of the code, line by line 
+# Using these characters lets you input all of the code, line by line 
 
 
 first_number = int(input('Type the first number: ')) ;\
