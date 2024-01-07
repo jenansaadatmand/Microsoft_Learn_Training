f3805e73ca749168047647c7837cfb1a0836eb08
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-07 11:28:11 -0500

    Update app_prompts_user_to_enter_list_of_planets.py

--- app_prompts_user_to_enter_list_of_planets.py
+++ app_prompts_user_to_enter_list_of_planets.py
@@ -14,7 +14,7 @@
 new_planet = ' '
 planets = []
 while new_planet.lower() != 'done':
-    if new_planet: # to check if user has entered a value
+    if new_planet: # To check if user has entered a value
         planets.append(new_planet) # first time it checks the value is empty ' '
     new_planet = input('Enter a new planet or done if done: ')
 print(planets) # presents the list of entered planets including the empty string for the first check due to the if statement 
