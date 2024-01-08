22998f15f54bc1541a90f0eff823658dedd7499c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-08 10:03:39 -0500

    Update app_prompts_user_to_enter_list_of_planets.py

--- app_prompts_user_to_enter_list_of_planets.py
+++ app_prompts_user_to_enter_list_of_planets.py
@@ -7,7 +7,7 @@
 
 # Starting with the variables you've just created, create a while loop. The while loop will run while new_planet is not set to done.
 # Inside the loop, check to see whether the new_planet variable contains a value, which should be the name of a planet. This is a quick way to see whether the user has entered a value. If they have, your code will append that value to the planets variable.
-# Complete the while loop by using input to prompt the user to enter a new planet name or done if they're done entering planet names. You'll store the value from input in the new_planet variable.
+# Complete the while loop by using the input to prompt the user to enter a new planet name or done if they're done entering planet names. You'll store the value from input in the new_planet variable.
 # Finally, outside of the while loop, print the list of planets by using print.
 # As you complete this part of the exercise, pay attention to tab levels to ensure code is run at the correct time.
 
