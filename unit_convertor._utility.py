aa2228659fc3b73b3147acc492d9f9326508e3bb
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-12 09:52:52 -0500

    Update unit_convertor._utility.py

--- unit_convertor._utility.py
+++ unit_convertor._utility.py
@@ -2,7 +2,7 @@
 # Converting parsecs to lightyears
 # The parsec (symbol: pc) is a unit of length used to measure the large distances to astronomical objects outside the Solar System, approximately equal to 3.26 light-years or 206,265 astronomical units (AU)
 # One parsed is 3.26 lightyears
-# So you will multipy parsecs by that value to determine lightyears
+# So you will multiply parsecs by that value to determine lightyears
 # create a variable named parsecs and set it to 11
 # Then add the code to perform the appropriate calculation and
 # Store the result in a variable named lightyears 
