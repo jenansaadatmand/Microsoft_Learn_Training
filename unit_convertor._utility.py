eda52a77c986dc607e6c9927dad2fbc106754301
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-12 09:52:44 -0500

    Update unit_convertor._utility.py

--- unit_convertor._utility.py
+++ unit_convertor._utility.py
@@ -4,7 +4,7 @@
 # One parsed is 3.26 lightyears
 # So you will multipy parsecs by that value to determine lightyears
 # create a variable named parsecs and set it to 11
-# Then add the code to perfom the appropriate calculation and
+# Then add the code to perform the appropriate calculation and
 # Store the result in a variable named lightyears 
 # Finally print the result on the screen with print 
 # So it displays a message which resembles the following: 
