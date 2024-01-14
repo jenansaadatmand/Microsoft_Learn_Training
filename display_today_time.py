f15786d5cdad42e62c0b333a71e42b3a5a8e3273
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-14 18:40:59 -0500

    Update display_today_time.py

--- display_today_time.py
+++ display_today_time.py
@@ -1,5 +1,5 @@
 # A program that displays today's date
-# use date object from the datetime library to access calendar information
+# Use date object from the datetime library to access calendar information
 # use the date.today()   use the method today() on the date object
  
 from datetime import date 
