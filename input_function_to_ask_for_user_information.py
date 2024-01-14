db13974d5659868b6a0673a23065d3a785d16f23
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-14 18:41:41 -0500

    Update input_function_to_ask_for_user_information.py

--- input_function_to_ask_for_user_information.py
+++ input_function_to_ask_for_user_information.py
@@ -1,5 +1,5 @@
 # Input function to ask for user information
-# store the input value in a variable
+# Store the input value in a variable
 # print the value of the variable back to the user 
 # \n is an escape character, enter new line
 # Using print() statements as debugging tools
