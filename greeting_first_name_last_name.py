74479f0c51094320bd38ba1adc16bf30d3e733bb
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-15 15:38:51 -0500

    Add files via upload

---
+++ greeting_first_name_last_name.py
@@ -0,0 +1,6 @@
+# Program that prints first name and last name and greets the user
+first_name = 'Christopher'
+last_name = 'Harrison'
+print(first_name + last_name)
+print()
+print('Hello ' + first_name + ' ' + last_name)
