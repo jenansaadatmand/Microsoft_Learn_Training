2935dca29bce23a3768ad4634d28ced16da7c53e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-15 16:19:32 -0500

    Add files via upload

---
+++ strings_manipulation.py
@@ -0,0 +1,7 @@
+# Program that manipulates strings
+sentence = 'The dog is named Jimmy'
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print()
+print(sentence.count('a'))
