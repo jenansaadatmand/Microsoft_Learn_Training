741550a66580b653d4c1c74ae9a58c2ce2d577a1
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-15 16:17:43 -0500

    Update user_input_greetings_printed.py

--- user_input_greetings_printed.py
+++ user_input_greetings_printed.py
@@ -5,7 +5,7 @@ print('Hello ' + first_name.capitalize() + ' ' + last_name.capitalize())
 
 print()
 
-# alternatively, 
+# Alternatively, 
 first_name = input("Please enter your first name: ")
 last_name = input("Please enter your last name: ")
 print("Hello " + first_name.capitalize() + ' ' + last_name.capitalize())
