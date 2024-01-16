ed0e3cf1bac17c8893e9473955db44caede55c3e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-16 11:26:33 -0500

    Update complex_conditional_logic.py

--- complex_conditional_logic.py
+++ complex_conditional_logic.py
@@ -26,7 +26,7 @@ else:
 # nested conditional logic:
 # You can nest if, elif, else statements to create even more complex programs
 # To nest conditions, indent the inner conditions
-# everything in at the same level of indentation will be run in the same code block
+# Everything in at the same level of indentation will be run in the same code block
 
 a = 16
 b = 25
