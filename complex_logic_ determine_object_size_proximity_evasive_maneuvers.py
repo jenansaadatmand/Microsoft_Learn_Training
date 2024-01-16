62ea654823597fab9195b36d56a66a796fb72411
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-16 11:25:37 -0500

    Update complex_logic_ determine_object_size_proximity_evasive_maneuvers.py

--- complex_logic_ determine_object_size_proximity_evasive_maneuvers.py
+++ complex_logic_ determine_object_size_proximity_evasive_maneuvers.py
@@ -4,7 +4,7 @@
 # If the object is both larger than the threshold 
 # and within 1000km of the ship evasive maneuvers will be required.
 # Create two variables: object_size and proximity. 
-#Set object_size to 10 to represent 10m3. Set proximity to 9000.
+# Set object_size to 10 to represent 10m3. Set proximity to 9000.
 # Then add the code to display a message saying Evasive maneuvers required 
 # if both object_size is greater than 5 and proximity is less than 1000. 
 # Otherwise display a message saying the Object poses no threat.
