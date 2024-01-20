c6612052f8b7053006c512b30d8cb955cd989c61
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-19 23:05:07 -0500

    Update formating_strings_using_variables.py

--- formating_strings_using_variables.py
+++ formating_strings_using_variables.py
@@ -1,6 +1,6 @@
 # Exercise: Formatting strings:
 # Knowing how to format strings is essential when you're presenting information from a program. There are a few different ways to accomplish this in Python. In this exercise, 
-# you use variables that hold key facts about gravity on various moons and then use them to format and print the information.
+# You use variables that hold key facts about gravity on various moons and then use them to format and print the information.
 
 # Create the variables
 # Start by creating three variables, name, gravity, and planet, and set them to the following values:
