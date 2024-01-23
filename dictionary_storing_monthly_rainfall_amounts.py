e27f4a4d8578bbbd670de335546c45b1c534c992
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-23 18:14:01 -0500

    Update dictionary_storing_monthly_rainfall_amounts.py

--- dictionary_storing_monthly_rainfall_amounts.py
+++ dictionary_storing_monthly_rainfall_amounts.py
@@ -7,7 +7,7 @@ rainfall = {'october': 3.5, 'november': 4.2, 'december':2.1}
 print(rainfall) # printing the full dictionary
 print()
 
-# Display the list of all rainfall using keys() and a for loo
+# Display the list of all rainfall using keys() and a for loop
 for key in rainfall.keys():
     print(f'{key}:{rainfall[key]}cm')
 print()
