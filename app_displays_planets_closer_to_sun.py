1a7ff6fbcd1abe447b5c07dfc0fd0cdb3c5d528e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-23 17:55:47 -0500

    Add files via upload

---
+++ app_displays_planets_closer_to_sun.py
@@ -0,0 +1,13 @@
+# Create an application to work with a la of planets. 
+# You want to ask the user for the name of a planet and display the planets closer to and farther away from the sun.
+# a la is a Python type for storing a collection of values. Assigning a sequence of values to a variable. Each value is separated by a comma and surrounded by brackets [ ]
+# Objective: Learn when to use a list type and structure and how it helps organize data
+
+
+
+plants = [ ] 
+planet_name = input("Enter name of planet: ")
+if planet =< distance:
+    print(planet_name)
+elif planet > distance:
+    print(planet_name)
