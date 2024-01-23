711f93c3f0c3ffe8b41599b3f7be06df7efb9334
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-23 17:56:50 -0500

    Update app_displays_planets_closer_to_sun.py

--- app_displays_planets_closer_to_sun.py
+++ app_displays_planets_closer_to_sun.py
@@ -1,6 +1,6 @@
-# Create an application to work with a la of planets. 
+# Create an application to work with a list of planets. 
 # You want to ask the user for the name of a planet and display the planets closer to and farther away from the sun.
-# a la is a Python type for storing a collection of values. Assigning a sequence of values to a variable. Each value is separated by a comma and surrounded by brackets [ ]
+# A list is a Python type for storing a collection of values. Assigning a sequence of values to a variable. Each value is separated by a comma and surrounded by brackets [ ]
 # Objective: Learn when to use a list type and structure and how it helps organize data
 
 
