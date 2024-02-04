18d8cffa0891b5f3f0c4333b89c9313fa0e9ccec
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-04 03:41:49 -0500

    Update Building_calculator_from _Microsoft.py

--- Building_calculator_from _Microsoft.py
+++ Building_calculator_from _Microsoft.py
@@ -1,5 +1,5 @@
 # Building a calculator from Microsoft Learn class introduction to Python
-# The characters ;\, at the end of the first two statements signal that there are many lines of code
+# The characters;\, at the end of the first two statements signal that there are many lines of code
 # Using these characters lets you input all of the code, line by line 
 
 
